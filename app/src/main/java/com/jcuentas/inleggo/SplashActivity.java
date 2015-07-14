d69df9bbec1f979e18b0ec932c75c40f114493aa
José Cuentas <jcuentast@gmail.com>
2015-07-14 17:41:01 -0500

    Se realizo la pantalla de login y se agrego efectos de traslation

---
+++ app/src/main/java/com/jcuentas/inleggo/SplashActivity.java
@@ -0,0 +1,45 @@
+package com.jcuentas.inleggo;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v7.app.ActionBarActivity;
+import android.transition.TransitionInflater;
+import android.view.View;
+import android.widget.ImageView;
+
+
+public class SplashActivity extends ActionBarActivity {
+
+    ImageView imvLogo;
+    @SuppressLint("NewApi")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+//        if (Build.VERSION.SDK_INT >= 21) {
+            getWindow().setSharedElementExitTransition(TransitionInflater.from(this).inflateTransition(R.transition.shared_element_transition_a));
+//        }
+        setContentView(R.layout.activity_splash);
+        imvLogo = (ImageView) findViewById(R.id.imv_logo);
+        imvLogo.setOnClickListener(new imgLogoOnClickListener());
+
+
+    }
+
+    private class imgLogoOnClickListener implements View.OnClickListener{
+        @SuppressLint("NewApi")
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+//            if (Build.VERSION.SDK_INT >= 21) {
+                v.setTransitionName("imageViewLogo");
+                ActivityOptionsCompat optionsCompat = ActivityOptionsCompat.makeSceneTransitionAnimation(SplashActivity.this, v, v.getTransitionName());
+                startActivity(intent, optionsCompat.toBundle());
+//            }else
+//                startActivity(intent);
+
+        }
+    }
+}
