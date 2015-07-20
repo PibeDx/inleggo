4073aefdc7ea12231bd08bd0a5e36a7ef5b2a2db
José Cuentas <jcuentast@gmail.com>
2015-07-20 17:39:30 -0500

    Agregando Service de Login

---
+++ app/src/main/java/com/jcuentas/inleggo/data/remote/LoginService.java
@@ -0,0 +1,11 @@
+package com.jcuentas.inleggo.data.remote;
+
+/**
+ * Created by Jose Cuentas Turpo on 20/07/2015 - 03:34 PM.
+ * E-mail: jcuentast@gmail.com
+ */
+public interface LoginService {
+//    @GET(Constantes.URL_LOGIN_SERVERS)
+//    void getServers(Callback <>)
+
+}
